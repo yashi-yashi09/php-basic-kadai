65b7a69d07726fa06aa0ec16007e439b9016da02
yashi-yashi09 <k.m.ak04129@gmail.com>
2024-08-31 15:33:57 +0900

    Add files via upload

---
+++ kadai_007/associative-array.php
@@ -0,0 +1,21 @@
+<!DOCTYPE html>
+<html lang="ja">
+
+<head>
+    <meta charset="UTF-8">
+    <title>PHP基礎編</title>
+</head>
+ 
+<body>
+    <p>
+        <?php
+            // 連想配列　値代入
+            $personal_data = ['name' => 'onion', 'price' => 200, 'weight' => 160];
+
+            print_r($personal_data);
+        ?>
+    </p>
+
+</body>
+
+</html>
